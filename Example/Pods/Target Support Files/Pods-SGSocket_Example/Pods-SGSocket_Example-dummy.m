3661a5583c9baa14f8f64b800fad897634a2b166
Sagar Ghag <sagar.ghag@thesunflowerlab.com>
2020-04-04 17:39:19 +0530

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/Pods-SGSocket_Example/Pods-SGSocket_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SGSocket_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SGSocket_Example
+@end
---
+++ Example/Pods/Target Support Files/SGSocket/SGSocket-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SGSocket : NSObject
+@end
+@implementation PodsDummy_SGSocket
+@end
